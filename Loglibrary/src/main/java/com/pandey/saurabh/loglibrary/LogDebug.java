53925ce8c168124a4e2e1f6a7657ca389fa03d8e
Saurabh Panday <saurabh@sparshmail.com>
2020-07-20 19:35:10 +0530

    First Library Testing

---
+++ Loglibrary/src/main/java/com/pandey/saurabh/loglibrary/LogDebug.java
@@ -0,0 +1,14 @@
+package com.pandey.saurabh.loglibrary;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "LogDebug";
+
+    public static  void  d(String message){
+
+        Log.d(TAG, message);
+    }
+
+}
